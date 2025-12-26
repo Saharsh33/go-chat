7f78b85e50290a70da2ab477dbcd510354b25f48
Prajwal Patil <prajwalspatil2006@gmail.com>
2025-12-27 00:15:25 +0530

    Added hub struct

--- internal/websocket/hub.go
+++ internal/websocket/hub.go
@@ -1,1 +1,15 @@
 package websocket
+
+type Hub struct {
+	Clients    map[*Client]bool
+	Register   chan *Client
+	Unregister chan *Client
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		Clients:    make(map[*Client]bool),
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+	}
+}
